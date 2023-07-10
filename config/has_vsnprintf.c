7dfac253f8aac50c18d05bc24a6f4feb61ab642b
Bill Allombert <Bill.Allombert@math.u-bordeaux.fr>
2023-07-10 19:59:58 +0200

    config/has_vsnprintf.c: fix warning

--- config/has_vsnprintf.c
+++ config/has_vsnprintf.c
@@ -2,8 +2,8 @@
 #include <stdarg.h>
 
 int main() { return 0; }
-int f(int i,...) { char s[1]; va_list ap; va_start(ap,i);
-  vsnprintf(s,1," ",ap); return 0;
+int f(int i,...) { char s[2]; va_list ap; va_start(ap,i);
+  vsnprintf(s,2," ",ap); return 0;
 }
 
 
