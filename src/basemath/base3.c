48cef68b07586ae768bba30206062f41c803a5ea
Karim Belabas <Karim.Belabas@math.u-bordeaux.fr>
2023-08-06 02:21:15 +0200

    add further heuristic (don't trust small result) in eval_sign_embed [#2474]
    Still not foolproof: if we have enough cancellation the floating point
    result is essentially random. A rigorous check will either require
    interval arithmetic (long shot) or remove the floating point step
    altogether (inefficient).
    
    N.B. Catches the
    
    bnf=bnfinit(x^10-5*x^9-1917*x^8-220484*x^7+1861684*x^6+179215404*x^5+7592279335*x^4-103008837215*x^3-4729077708495*x^2-100465756135620*x+46969787343312,1);
    
    problem (after reverting the small_norm patch p0 -> p0^e)

--- src/basemath/base3.c
+++ src/basemath/base3.c
@@ -1737,8 +1737,9 @@ eval_sign_embed(GEN z)
   if (typ(z) == t_REAL)
   {
     long l = realprec(z);
-    if (l <= LOWDEFAULTPREC || (l == LOWDEFAULTPREC + 1 && !z[LOWDEFAULTPREC]))
-      return -1; /* dubious, fail */
+    if (l <= LOWDEFAULTPREC
+      || (l == LOWDEFAULTPREC + 1 && !z[l-1])) return -1; /* dubious, fail */
+    if (expo(z) < 16 - bit_accuracy(l)) return -1; /* same */
   }
   return (signe(z) < 1)? 1: 0;
 }
