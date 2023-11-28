943e021f65b6de85a852b8efe0e941138c9212af
Karim Belabas <Karim.Belabas@math.u-bordeaux.fr>
2023-11-28 10:47:30 +0100

    doc improvements + document \uv in ?\

--- src/language/gplib.c
+++ src/language/gplib.c
@@ -338,6 +338,7 @@ slash_commands(void)
 \\t      : print the list of PARI types\n\
 \\u      : print the list of user-defined functions\n\
 \\um     : print the list of user-defined member functions\n\
+\\uv     : print the list of user-defined variables, excluding closures\n\
 \\v      : print current version of GP\n\
 \\w {nf} : write to a file\n\
 \\x {n}  : print complete inner structure of result\n\
