b8d7132f836d4524ba295ff45b965a91c003b5e9
Bill Allombert <Bill.Allombert@math.u-bordeaux.fr>
2024-01-17 23:41:06 +0100

    fincke_pohst: detect precision error in inexact Gram matrix

--- src/basemath/bibli1.c
+++ src/basemath/bibli1.c
@@ -1962,7 +1962,14 @@ fincke_pohst(GEN a, GEN B0, long stockmax, long PREC, FP_chk_fun *CHECK)
       if (CHECK) pari_err_TYPE("fincke_pohst [dimension 0]", a);
       retmkvec3(gen_0, gen_0, cgetg(1,t_MAT));
     }
-    u = lllfp(a, 0.75, LLL_GRAM | LLL_IM);
+    if (isinexact(a))
+    {
+      GEN C = RgM_Cholesky(a, prec);
+      if (!C) return gc_NULL(av);
+      u = lllfp(C, 0.75, LLL_IM);
+    }
+    else
+      u = lllfp(a, 0.75, LLL_GRAM | LLL_IM);
     if (lg(u) != lg(a)) return gc_NULL(av);
     r = qf_RgM_apply(a,u);
     i = gprecision(r);
