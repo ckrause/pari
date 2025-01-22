ckrause/pari

commit 0595acc433bd0d7b1fb080c6d7b0065d9eaf6911

4- vecsearch(t_LIST,) would raise a type error [#2535]

src/basemath/bibli2.c
@@ -1869,12 +1869,23 @@ long
 vecsearch(GEN v, GEN x, GEN k)
 {
   pari_sp av = avma;
+  long r;
   void *E;
   int (*CMP)(void*,GEN,GEN) = sort_function(&E, v, k);
-  long r, tv = typ(v);
-  if (tv == t_VECSMALL)
-    x = (GEN)itos(x);
-  else if (!is_matvec_t(tv)) pari_err_TYPE("vecsearch", v);
+  switch(typ(v))
+  {
+    case t_VECSMALL: x = (GEN)itos(x); break;
+    case t_VEC: case t_COL: break;
+    case t_LIST:
+      if (list_typ(v)==t_LIST_RAW)
+      {
+        v = list_data(v); if (!v) v = cgetg(1, t_VEC);
+        break;
+      }
+      /* fall through */
+    default:
+      pari_err_TYPE("vecsearch", v);
+  }
   r = CMP? gen_search(v, x, E, CMP): key_search(v, x, k);
   return gc_long(av, r < 0? 0: r);
 }

src/test/32/sort
@@ -30,6 +30,9 @@ Vecsmall([1, 2, 3, 4])
 0
 3
 List([1, 2, 3, 4])
+0
+3
+0
 [1, 2, 3, 4]
 Vecsmall([4, 6, 2, 1])
 [4, 3, 2, 1]
@@ -41,4 +44,4 @@ List([])
   ***   at top-level: vecsort(Vecsmall(1),1)
   ***                 ^----------------------
   *** vecsort: incorrect type in sort_function (t_VECSMALL).
-Total time spent: 1
+Total time spent: 0

src/test/in/sort
@@ -25,7 +25,10 @@ T=vecsort(Vecsmall([4,3,2,1]))
 vecsearch(T,-1)
 vecsearch(T,5)
 vecsearch(T,3)
-vecsort(List([4,3,2,1]))
+T=vecsort(List([4,3,2,1]))
+vecsearch(T,-1)
+vecsearch(T,3)
+vecsearch(List(),1)
 v=[4,3,4,1,4,2,3];
 vecsort(v,, 8)
 vecsort(v,, 8+1)

commit aa64fbc542fca0ce8008cfbaa0b45adf97e3e6d3

5- vecsearch(t_MAT, t_COL or t_VEC) -> error
introduced in 0595acc433

src/basemath/bibli2.c
@@ -1845,7 +1845,7 @@ vecsearch(GEN v, GEN x, GEN k)
   switch(typ(v))
   {
     case t_VECSMALL: x = (GEN)itos(x); break;
-    case t_VEC: case t_COL: break;
+    case t_VEC: case t_COL: case t_MAT: break;
     case t_LIST:
       if (list_typ(v)==t_LIST_RAW)
       {

src/test/32/sort
@@ -38,10 +38,13 @@ Vecsmall([4, 6, 2, 1])
 [4, 3, 2, 1]
 Vecsmall([1, 2, 6, 4])
 List([])
+3
+2
+0
   ***   at top-level: vecsearch(Vecsmall(1),1,1)
   ***                 ^--------------------------
   *** vecsearch: incorrect type in sort_function (t_VECSMALL).
   ***   at top-level: vecsort(Vecsmall(1),1)
   ***                 ^----------------------
   *** vecsort: incorrect type in sort_function (t_VECSMALL).
-Total time spent: 0
+Total time spent: 1

src/test/in/sort
@@ -36,6 +36,11 @@ vecsort(v,, 8+4)
 vecsort(v,, 8+4+1)
 vecsort(List(),,4)
 
+v=vecsort([3,0,2; 1,0,2]);
+vecsearch(v, [3,1]~)
+vecsearch(v, [2,2])
+vecsearch(v, [2,1])
+
 \\Errors
 vecsearch(Vecsmall(1),1,1)
 vecsort(Vecsmall(1),1)