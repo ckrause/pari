4a14368320ae1b2e6c415e6c3745ea5955eabf4c
Karim Belabas <Karim.Belabas@math.u-bordeaux.fr>
2025-02-10 22:23:11 +0100

    11- vectorsmall(5, i, 2^63-1).normfu -> SEGV

--- src/language/members.c
+++ src/language/members.c
@@ -234,7 +234,7 @@ member_r2(GEN x) { return gel(member_sign(x), 2); }
 GEN
 member_normfu(GEN x) /* norm of fundamental unit */
 {
-  if (lg(x)!=6 || typ(gel(x,1))!=t_INT || typ(gel(x,2))!=t_VEC)
+  if (typ(x)!=t_VEC || lg(x)!=6 || typ(gel(x,1))!=t_INT || typ(gel(x,2))!=t_VEC)
     member_err("normfu",x);
   return gel(x,5);
 }
