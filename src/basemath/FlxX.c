d8d28e18092fff5b3dec64da6b6206a963565692
Karim Belabas <Karim.Belabas@math.u-bordeaux.fr>
2025-04-22 02:57:32 +0200

    FlxqX_divrem_basecase: GC cleanup

--- src/basemath/FlxX.c
+++ src/basemath/FlxX.c
@@ -826,13 +826,11 @@ FlxqX_powu(GEN V, ulong n, GEN T, ulong p)
 static GEN
 FlxqX_divrem_basecase(GEN x, GEN y, GEN T, ulong p, ulong pi, GEN *pr)
 {
-  long vx, dx, dy, dz, i, j, sx, lr;
-
-  pari_sp av0, av, tetpil;
-  GEN z,p1,rem,lead;
+  long vx = varn(x), dx = degpol(x), dy = degpol(y), dz, i, j, sx, lr;
+  pari_sp av0, av;
+  GEN z, p1, rem, lead;
 
   if (!signe(y)) pari_err_INV("FlxqX_divrem",y);
-  vx=varn(x); dy=degpol(y); dx=degpol(x);
   if (dx < dy)
   {
     if (pr)
@@ -866,11 +864,11 @@ FlxqX_divrem_basecase(GEN x, GEN y, GEN T, ulong p, ulong pi, GEN *pr)
   gel(z,dz) = lead? gc_upto(av, Flxq_mul_pre(p1,lead, T,p,pi)): gcopy(p1);
   for (i=dx-1; i>=dy; i--)
   {
-    av=avma; p1=gel(x,i);
+    av = avma; p1 = gel(x,i);
     for (j=i-dy+1; j<=i && j<=dz; j++)
       p1 = Flx_sub(p1, Flx_mul_pre(gel(z,j),gel(y,i-j),p,pi),p);
     if (lead) p1 = Flx_mul_pre(p1, lead,p,pi);
-    tetpil=avma; gel(z,i-dy) = gc_GEN_unsafe(av,tetpil,Flx_rem_pre(p1,T,p,pi));
+    gel(z,i-dy) = gc_uptoleaf(av, Flx_rem_pre(p1,T,p,pi));
   }
   if (!pr) { guncloneNULL(lead); return z-2; }
 
@@ -880,7 +878,7 @@ FlxqX_divrem_basecase(GEN x, GEN y, GEN T, ulong p, ulong pi, GEN *pr)
     p1 = gel(x,i);
     for (j=0; j<=i && j<=dz; j++)
       p1 = Flx_sub(p1, Flx_mul_pre(gel(z,j),gel(y,i-j),p,pi),p);
-    tetpil=avma; p1 = Flx_rem_pre(p1, T,p,pi); if (lgpol(p1)) { sx = 1; break; }
+    p1 = Flx_rem_pre(p1, T,p,pi); if (lgpol(p1)) { sx = 1; break; }
     if (!i) break;
     set_avma(av);
   }
@@ -890,17 +888,16 @@ FlxqX_divrem_basecase(GEN x, GEN y, GEN T, ulong p, ulong pi, GEN *pr)
     if (sx) return gc_NULL(av0);
     return gc_const((pari_sp)rem, z-2);
   }
-  lr=i+3; rem -= lr;
+  lr=i+3; rem -= lr; av = (pari_sp)rem;
   rem[0] = evaltyp(t_POL) | _evallg(lr);
   rem[1] = z[-1];
-  p1 = gc_GEN_unsafe((pari_sp)rem,tetpil,p1);
-  rem += 2; gel(rem,i) = p1;
+  rem += 2; gel(rem,i) = gc_uptoleaf(av, p1);
   for (i--; i>=0; i--)
   {
-    av=avma; p1 = gel(x,i);
+    av = avma; p1 = gel(x,i);
     for (j=0; j<=i && j<=dz; j++)
       p1 = Flx_sub(p1, Flx_mul_pre(gel(z,j),gel(y,i-j),p,pi), p);
-    tetpil=avma; gel(rem,i) = gc_GEN_unsafe(av,tetpil, Flx_rem_pre(p1, T,p,pi));
+    gel(rem,i) = gc_uptoleaf(av, Flx_rem_pre(p1, T,p,pi));
   }
   rem -= 2;
   guncloneNULL(lead);
